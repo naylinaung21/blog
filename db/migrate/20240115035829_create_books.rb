743c871b0f4a9c4c1285a1263f4a4668cbd4652a
Nay Lin Aung <naylinaung@istd.co.jp>
2024-01-15 10:33:45 +0630

    feat():=> CRUD for Book

---
+++ db/migrate/20240115035829_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.decimal :price, precision: 10, scale: 2
+      t.references :author, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
